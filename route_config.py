762fc94de7c5cd4fef81e649a52b3f94bdd3555f
thanakritfluk <thanakrit.da@ku.th>
2021-01-23 00:46:35 +0700

    Test

--- route_config.py
+++ route_config.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from models.lipstick import Lipstick
+from flask import Flask, request
+from "/models/lipstick.py" import Lipstick
+from detection.detector import face_detect
 # app reference
 app = Flask(__name__)
 
@@ -16,6 +17,21 @@ def insert_lipstick():
     lipstick = Lipstick('FlukLip', '#EBA38B', 500)
     insert = lipstick.insert()
     return("Success insert id:"+str(insert), 200)
+
+
+@app.route('/api/prediction/lipstick', methods=['POST'])
+def predict_lipstick():
+    # check if the post request has the file part
+    if 'ref_face' not in request.files:
+        return {"detail": "No file found"}, 400
+    ref_face = request.files['ref_face']
+    if ref_face.filename == '':
+        return {"detail": "Invalid file or filename missing"}, 400
+    file_type = (ref_face.content_type).split('/')[1]
+    file_path = "./image/input/source." + file_type
+    ref_face.save(file_path)
+    face_detect(file_path,file_type)
+    return "Success"
 
 
 # This method returns lipstick
