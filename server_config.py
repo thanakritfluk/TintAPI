519919eb22a317eeb0531f783b4cfdd47645bf4f
thanakritfluk <thanakrit.da@ku.th>
2021-01-23 16:44:57 +0700

    Add if to define main app state for run server flask

--- server_config.py
+++ server_config.py
@@ -6,4 +6,6 @@ app.debug = True
 host = os.environ.get('IP', '0.0.0.0')
 port = int(os.environ.get('PORT', 8080))
 DB.init()
-app.run(host=host, port=port)
+
+if __name__ == "__main__"
+    app.run(host=host, port=port)
